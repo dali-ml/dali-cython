fffdc16eb02c9bb1ff6ac8a6d0165fcc2c90b269
Szymon Sidor <szymon.sidor@gmail.com>
2015-05-17 17:23:34 -0400

    The thing now compiles on Fedora - but more madness awaits (dynamic linking errors on import)

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-from Cython.Build import cythonize
-from Cython.Build.Dependencies import create_extension_list
+import distutils.ccompiler
+import distutils.sysconfig
 
-modname     = "test_dali"
 from os.path import join
 from os import environ
 
+from distutils.core import setup
+from Cython.Distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+modname     = "test_dali"
+
+environ["CC"] = "clang++"
+environ["CXX"] = "clang++"
+
 DALI_DIR = environ["DALI_HOME"]
+
+compiler = distutils.ccompiler.new_compiler()
+
+distutils.sysconfig.customize_compiler(compiler)
+
+BLACKLISTED_COMPILER_SO = [
+  '-Wp,-D_FORTIFY_SOURCE=2'
+]
+
+build_ext.compiler = compiler
 
 ext_modules = [Extension(
     name=modname,
@@ -16,8 +31,13 @@ ext_modules = [Extension(
       "test_dali.pyx"],
     language='c++',
     extra_compile_args=[
-        '-stdlib=libc++',
-        '-std=c++11'],
+        '-std=c++11',
+        '-fPIC'
+    ],
+
+    undef_macros= [
+      "_FORTIFY_SOURCE",
+    ],
     libraries=["protobuf"],
      extra_objects=[
         join(DALI_DIR, "build/dali/libdali.a"),
@@ -30,36 +50,20 @@ ext_modules = [Extension(
         join(DALI_DIR, "third_party/SQLiteCpp/include"),
         join(DALI_DIR, "third_party/json11"),
         "/usr/local/include/eigen3",
-        "/usr/include/eigen3"
+        "/usr/include/eigen3",
       ]
      )]
 
-# ext_options = {
-#   "language": "c++",
-#   "extra_compile_args": [
-#     '-stdlib=libc++',
-#     '-std=c++11'],
-#   "libraries": ["protobuf"],
-#   "extra_objects":[
-#     join(DALI_DIR, "build/dali/libdali.a"),
-#     join(DALI_DIR, "build/protobuf/libproto.a"),
-#     join(DALI_DIR, "build/third_party/SQLiteCpp/libSQLiteCpp.a")
-#   ],
-#   "include_dirs":[
-#     DALI_DIR,
-#     join(DALI_DIR, "third_party/SQLiteCpp/include"),
-#     "/usr/local/include/eigen3"
-#   ]
-# }
-
-# modules, depends = create_extension_list(["test_dali.pyx", "*/*/*.pyx"])
-
-# for module in modules:
-#   for key, value in ext_options.items():
-#     setattr(module, key, value)
-
+class nonbroken_build_ext(build_ext):
+    def build_extensions(self, *args, **kwargs):
+        new_compiler_so = []
+        for arg in self.compiler.compiler_so:
+            if arg not in BLACKLISTED_COMPILER_SO:
+                new_compiler_so.append(arg)
+        self.compiler.compiler_so = new_compiler_so
+        super(nonbroken_build_ext, self).build_extensions(*args, **kwargs)
 setup(
   name = modname,
-  cmdclass = {"build_ext": build_ext},
+  cmdclass = {"build_ext": nonbroken_build_ext},
   ext_modules = ext_modules
 )

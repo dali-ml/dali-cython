c2f29a1eec02548c98597e50d33114808d4f0915
Szymon Sidor <szymon.sidor@gmail.com>
2015-08-02 17:11:45 -0700

    Array now stores dtype

--- preprocessor_utils.py
+++ preprocessor_utils.py
@@ -11,4 +11,4 @@ def typed_expression(pyp, replacable_expression, replacable_type, code):
     typefixed = code.replace('TYPED_EXPRESSION', '(<%s[double]>(%s))' % (replacable_type, replacable_expression))
     pyp.indent(typefixed)
     pyp.indent('else:')
-    pyp.indent('    raise ValueError("Invalid dtype:" + self.dtype + " (should be one of int32, float32, float64)")')
+    pyp.indent('    raise ValueError("Invalid dtype:" + str(self.dtype) + " (should be one of int32, float32, float64)")')

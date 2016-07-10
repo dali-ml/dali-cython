f56b06d43e711bec0bcd7931a72f22579e37679d
Szymon Sidor <szymon.sidor@gmail.com>
2016-07-10 00:36:03 -0700

    added the rest of unary ops

--- src/cython/dali/array/op/unary.pyx
+++ src/cython/dali/array/op/unary.pyx
@@ -2,3 +2,42 @@ from ..array cimport AssignableArray, Array, CAssignableArray, CArray
 
 cpdef AssignableArray sigmoid(Array a):
     return AssignableArray.wrapc(c_sigmoid(a.o))
+
+cpdef AssignableArray tanh(Array a):
+    return AssignableArray.wrapc(c_tanh(a.o))
+
+cpdef AssignableArray relu(Array a):
+    return AssignableArray.wrapc(c_relu(a.o))
+
+cpdef AssignableArray eltinv(Array a):
+    return AssignableArray.wrapc(c_eltinv(a.o))
+
+cpdef AssignableArray exp(Array a):
+    return AssignableArray.wrapc(c_exp(a.o))
+
+cpdef AssignableArray log(Array a):
+    return AssignableArray.wrapc(c_log(a.o))
+
+cpdef AssignableArray log_or_zero(Array a):
+    return AssignableArray.wrapc(c_log_or_zero(a.o))
+
+cpdef AssignableArray abs(Array a):
+    return AssignableArray.wrapc(c_abs(a.o))
+
+cpdef AssignableArray sign(Array a):
+    return AssignableArray.wrapc(c_sign(a.o))
+
+cpdef AssignableArray square(Array a):
+    return AssignableArray.wrapc(c_square(a.o))
+
+cpdef AssignableArray softplus(Array a):
+    return AssignableArray.wrapc(c_softplus(a.o))
+
+cpdef AssignableArray cube(Array a):
+    return AssignableArray.wrapc(c_cube(a.o))
+
+cpdef AssignableArray sqrt(Array a):
+    return AssignableArray.wrapc(c_sqrt(a.o))
+
+cpdef AssignableArray rsqrt(Array a):
+    return AssignableArray.wrapc(c_rsqrt(a.o))

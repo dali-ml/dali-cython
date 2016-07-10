1ca2bf58bbe307cbb866c7efe1d645ca961ce620
Jonathan Raiman <jonathanraiman@gmail.com>
2016-07-09 19:14:49 -0700

    add stub of Tensor class

--- src/cython/dali/tensor/tensor.pyx
+++ src/cython/dali/tensor/tensor.pyx
@@ -1,2 +1,39 @@
-cpdef int do_tensor_stuff(int x):
-    return 2 * x
+import numpy as np
+# Numpy must be initialized. When using numpy from C or Cython you must
+# _always_ do that, or you will have segfaults
+c_np.import_array()
+
+cdef class Tensor:
+    def __cinit__(Tensor self, vector[int] shape, dtype=np.float32, preferred_device=None):
+        cdef Device device
+        device = ensure_device(preferred_device)
+        self.o = CTensor(shape, dtype_np_to_dali(dtype), device.o)
+
+    @staticmethod
+    cdef Tensor wrapc(CTensor o):
+        ret = Tensor([])
+        ret.o = o
+        return ret
+
+    cdef c_np.NPY_TYPES cdtype(Tensor self):
+        return dtype_dali_to_c_np(self.o.dtype())
+
+    property dtype:
+        def __get__(Tensor self):
+            return dtype_dali_to_np(self.o.dtype())
+
+    property w:
+        def __get__(Tensor self):
+            return Array.wrapc(self.o.w)
+
+    property dw:
+        def __get__(Tensor self):
+            return Array.wrapc(self.o.dw)
+
+    property shape:
+        def __get__(Tensor self):
+            return tuple(self.o.shape())
+
+    property preferred_device:
+        def __get__(Tensor self):
+            return Device.wrapc(self.o.preferred_device())
--- src/python/dali/__init__.py
+++ src/python/dali/__init__.py
@@ -1,6 +1,7 @@
 from numpy import float32, float64, int32
 
 from .array.array import Array
+from .tensor.tensor import Tensor
 from .array.memory.device import (
     set_default_device,
     default_device,
